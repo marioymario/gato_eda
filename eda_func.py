a9ecd4d9cde0a9a7c035406b9efbfe7e67c24f91
gato <mario.hevia@gmail.com>
2022-09-29 09:06:43 -0700

    Add 3 new functions, invalid_df, siftdatatype, and filler of the nans, each is documented related use.

--- eda_func.py
+++ eda_func.py
@@ -63,3 +63,107 @@ def basic_info(dataframe):
     print(df.info())
     
     
+def invalid_df(dataframe: pd.DataFrame):
+    """
+    Take a pandas df as argument, looks for the items 
+    in an invalid list. returns a pd df with
+    the columns: column, nulls, invalids, 
+    and the unique values.
+    Args:
+        df(pd.DataFrame): a pdDataFrame.
+    """
+    # Running validation on the argument recieved
+    assert type(dataframe) == pd.DataFrame, f'{dataframe}, is not a pandas df.'
+    df = dataframe
+    invalid_list = [np.nan, None, [], {}, 'NaN', 'Null','NULL','None','NA','?','-', '--','.','', ' ', '   ']
+    columnas_con_invalidos = []
+    nan_or_nones = []
+    invalids = []
+    uniques = []
+    invalid_dict = {
+        'column': columnas_con_invalidos,
+        'nulls': nan_or_nones,
+        'invalids': invalids, 
+        'unique_item': []
+    }
+    for c in df.columns:
+        string_null = np.array([x in invalid_list[2:] for x in df[c]])
+        columnas_con_invalidos.append(c)
+        nan_or_nones.append((df[c].isnull().sum()))
+        invalids.append(string_null.sum())
+        uniques.append(df[c].unique())
+        invalid_dict = {
+        'columns': columnas_con_invalidos,
+        'nulls': nan_or_nones,
+        'invalids': invalids,
+        'unique_item': uniques
+        }
+        
+    result = pd.DataFrame(invalid_dict)
+    return(result.head(len(df.columns)))
+    
+def siftdatatype(dataframe: pd.DataFrame):
+    """
+    Recive a pandas data frame as an argument
+    returns a dictionary with a message and 
+    keys categorical and numerical, the values
+    are two list each corresponding to the keys.
+    Args:
+        df(pd.DataFrame): a pdDataFrame.
+    """
+    # Running validation on the argument recieved
+    assert type(dataframe) == pd.DataFrame, f'{dataframe}, is not a pandas df.'
+    df = dataframe
+    # groupping columns with numbers, dtypes can increase in types len.
+    num_features = df.select_dtypes(['int64', 'float64']).columns.to_list()
+    # groupping object columns
+    cat_features = df.select_dtypes(['object']).columns.to_list()
+    lenghts = [len(num_features), len(cat_features)]
+    elementos = [num_features, cat_features]
+    results = {
+        "message" : f"{lenghts[0]} features are numerical and {lenghts[1]} features are categorical.",
+        "numerical" : elementos[0], 
+        "categorical" : elementos[1]
+    }
+    return results
+
+def filler_of_the_nans(technique, df, list_to_fill):
+    """
+    Fill in nans of a fill_list from a pandas df,
+    the fill list should be defined by the technique to use. 
+    The options are mean, median, mode or interpolation.
+
+    Args:
+        technique (list): a technique to use, from the list
+        df(pd.DataFrame): a pdDataFrame.
+        fill_list (pd.Series): pd.Series or list of Series with
+        missing values. 
+        
+    """
+    # Running validation on the argument recieved
+    tecnicas = ('mean', 'median', 'interpolation', 'mode')
+    assert type(df) == pd.DataFrame, f'{df} is not a pandas df.'
+    assert technique in tecnicas, f'{technique} not in options:\
+        [mean|median|interpolation|mode]'
+
+    # Deffining and populating a dataframe
+    dff = pd.DataFrame()
+    dff = df.copy()
+
+    if technique == 'mean':
+        for i in list_to_fill:
+            dff.loc[:, i] =  dff.loc[:, i].fillna(dff.loc[:, i].mean())
+        return dff
+    elif technique == 'median':
+        for i in list_to_fill:
+            dff.loc[:, i] =  dff.loc[:, i].fillna(dff.loc[:, i].median())
+        return dff
+    elif technique == 'interpolation':
+        for i in list_to_fill:
+            dff.loc[:, i] =  dff.loc[:, i].fillna(dff.loc[:, i].interpolate())
+        return dff
+    elif technique == 'mode':
+        for i in list_to_fill:
+            dff.loc[:, i] =  dff.loc[:, i].fillna(dff.loc[:, i].mode()[0])
+        return dff
+    else: return dff
