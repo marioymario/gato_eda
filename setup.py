9131bc406ab02c8f39199dc8f7cd912b9b115cc3
gato <mario.hevia@gmail.com>
2023-07-14 17:33:51 -0700

    Add code and fix code issued on get_invalid_values()

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as fh:
     long_description = '\n' + fh.read()
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'Functions for exploratory data analysis'
 LONG_DESCRIPTION = 'A set of functions that help to clean and analyse data builded on top of pandas, numpy, matplot.pyplot, seaborn, statsmodels, and scipy.'
 
